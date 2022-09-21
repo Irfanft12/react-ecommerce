71af942cbad9042019738f938b62c63bbb5299c8
Irfan Ahmad <irfanft12@gmail.com>
2022-09-21 19:50:34 +0530

    21 september work

--- src/components/cart/Cart.jsx
+++ src/components/cart/Cart.jsx
@@ -1,11 +1,10 @@
 import React from 'react'
 import { Container, Grid, Button, Typography } from '@material-ui/core'
 import useStyles from "./styles"
+import CartItem from './cart-item/CartItem'
 
 
 const Cart = ({ cart }) => {
-    console.log(cart)
-    // console.log(cart.line_items.length)
     const classes = useStyles()
     const isEmpty = !cart.total_items
 
@@ -16,8 +15,22 @@ const Cart = ({ cart }) => {
     const FilledCart = () => (
         <>
             <Grid container spacing={3}>
-                {cart.total_items}
+                {cart.line_items.map((item) => (
+                    <Grid item xs={12} sm={4} key={item.id} >
+                        <CartItem item={item} />
+                        {/* <div>{item.name}</div> */}
+                    </Grid>
+                ))}
             </Grid>
+            <div className={classes.cardDetails}>
+                <Typography variant='h4'>
+                    Subtotal: { cart.subtotal.formatted_with_symbol }
+                </Typography>
+                <div>
+                    <Button className={classes.emptyButton} size="large" type="button" variant="contained" color="secondary">Empty Cart</Button>
+                    <Button className={classes.checkoutButton} size="large" type="button" variant="contained" color="primary">Checkout</Button>
+                </div>
+            </div>
         </>
     )
 
